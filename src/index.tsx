2f8828caf9baf1b701ec623e958ba28ddca32080
Marcin-Migdal <mortimor132@gmail.com>
2023-11-23 13:16:59 +0100

    further improvements

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import { createRoot } from "react-dom/client";
 import React from "react";
+import App from "./App";
 
-import { App } from "./components";
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement as HTMLElement);
